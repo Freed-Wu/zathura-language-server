f311f2fbb76e1e2be1117a7bd7ad53ad921632ab
Wu, Zhenyu <wuzhenyu@ustc.edu>
2023-12-02 18:04:10 +0800

    :recycle: Respect json schema

---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+r"""Test utils."""
+from zathura_language_server.utils import get_schema
+
+
+class Test:
+    r"""Test."""
+
+    @staticmethod
+    def test_get_schema() -> None:
+        r"""Test get schema.
+
+        :rtype: None
+        """
+        assert len(
+            get_schema()
+            .get("properties", {})
+            .get("recolor", {})
+            .get("description", "")
+            .splitlines()
+        )
